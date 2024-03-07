a8731fcc2a19c2cf58a77b76d8bc346b29958015
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-03-07 12:17:14 +0000

    Starting to get some object templates together

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ pygame.init()
 FPS_CLOCK = pygame.time.Clock()
 
 
-print("Hello World")
+class Cat:
+    def __init__(self):
+        ...
 
 
 class Game:
     def __init__(self):
+        self.__running = True
+        # self.__surface = pygame.display.set_mode(FULLSCREEN)
+
+    def __check_events(self):
         ...
+
+    def __process(self):
+        ...
+
+    def run(self) -> None:
+        while self.__running:
+            ...
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
