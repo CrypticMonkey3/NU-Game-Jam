6d76e07b19e6a60f1addadd0de3d72efeed6465d
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-03-08 18:46:33 +0000

    Need to fix, text display, and paddles need to be offset to get them in the centre of screen

--- main.py
+++ main.py
@@ -10,26 +10,80 @@ WHITE = (255, 255, 255)
 
 
 class SpriteManager:
+    __instance = None
+
     def __init__(self):
-        ...
+        self.__object_pool: Dict[str: List[object]] = {}
+
+    def add_objects(self, obj_label: str, obj, quantity: int = 1, *args) -> None:
+        """
+        Creates an x amount of objects and adds it to the pool of objects
+        :param str obj_label: The label to give to the object
+        :param obj: The type of object to create
+        :param int quantity: The number of objects to create
+        :return: None
+        """
+        self.__object_pool[obj_label] = [obj(*args) for _ in range(quantity)]
+
+    @property
+    def object_pool(self):
+        return self.__object_pool
 
     def __new__(cls):
-        ...
+        if not hasattr(cls, "instance"):
+            cls.__instance = super(SpriteManager, cls).__new__(cls)
+
+        return cls.__instance
+
+
+class Text:
+    def __init__(self, *args):
+        self._surface: pygame.Surface = args[0]
+        self._message: str = args[1]
+        self._font_dir: str = args[2]
+        self._size: int = args[3]
+        self._pos: Tuple[int, int] = args[4]
+        self._color: Tuple[int, int, int] = args[5]
+        self._font_surf: pygame.Surface = Font(args[2], args[3]).render(args[1], False, args[5]).convert_alpha(args[0])
+
+    def update_message(self, new_message: str) -> None:
+        """
+        Updates the message in this text surface
+        :param str new_message: The new message
+        :return: None
+        """
+        filler_surf = self._font_surf.copy()
+        filler_surf.fill(WHITE)
+        self._surface.blit(filler_surf, self._pos)
+        pygame.display.update(Rect(self._pos[0], self._pos[1], self._font_surf.get_width(),
+                                   self._font_surf.get_height()))
+
+        self._font_surf = Font(self._font_dir, self._size).render(new_message, False, self._color).convert_alpha(self._surface)
+        self.move_pos(self._pos[0], self._pos[1])
+        self._surface.blit(self._font_surf, self._pos)
+        pygame.display.update(Rect(self._pos[0], self._pos[1], self._font_surf.get_width(),
+                                   self._font_surf.get_height()))
+
+    def move_pos(self, x, y):
+        self._pos = (
+            self._surface.get_width() + x - self._font_surf.get_width() if x < 0 else self._pos[0],
+            self._surface.get_height() + y - self._font_surf.get_height() if y < 0 else self._pos[1]
+        )
 
 
 class Sprite:
-    def __init__(self, surface: pygame.Surface, image_dirs: List[str], init_pos: Tuple[int, int]):
-        self._surface = surface
-        self._image = pygame.image.load(image_dirs[0]).convert_alpha(surface)
+    def __init__(self, *args):
+        self._surface: pygame.Surface = args[0]
+        self._image: pygame.Surface = pygame.image.load(args[1][0]).convert_alpha(args[0])
         self._filler_surf = self._image.copy()
         self._filler_surf.fill(WHITE)
-        self._start_pos = init_pos
+        self._start_pos: Tuple[int, int] = args[2]
 
-        self._image_outline = image_dirs[1]
-        if image_dirs[1]:
-            self._image_outline = pygame.image.load(image_dirs[1]).convert_alpha(surface)
+        self._image_outline = args[1][1]
+        if args[1][1]:
+            self._image_outline = pygame.image.load(args[1][1]).convert_alpha(args[0])
 
-        self._rect = Rect(init_pos[0], init_pos[1], self._image.get_width(), self._image.get_height())
+        self._rect = Rect(args[2][0], args[2][1], self._image.get_width(), self._image.get_height())
 
     def move_pos(self, x: int, y: int):
         """
@@ -61,13 +115,13 @@ class Sprite:
 
 
 class Cat(Sprite):
-    def __init__(self, surface: pygame.Surface, image_dirs: List[str], init_pos: Tuple[int, int]):
-        super().__init__(surface, image_dirs, init_pos)
+    def __init__(self, *args):
+        super().__init__(*args)
 
 
 class Player(Sprite):
-    def __init__(self, surface: pygame.Surface, image_dirs: List[str], init_pos: Tuple[int, int]):
-        super().__init__(surface, image_dirs, init_pos)
+    def __init__(self, *args):
+        super().__init__(*args)
         # --- Rotate paddles --- #
         self._image = pygame.transform.rotate(self._image, 90)
         self._image_outline = pygame.transform.rotate(self._image_outline, 90)
@@ -78,23 +132,35 @@ class Player(Sprite):
 
 
 class Ball(Sprite):
-    def __init__(self, surface: pygame.Surface, image_dirs: List[str], init_pos: Tuple[int, int]):
-        super().__init__(surface, image_dirs, init_pos)
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.__speed = 1
 
 
 class Game:
     def __init__(self):
         self.__running = True
         self.__surface = pygame.display.set_mode([0, 0], pygame.FULLSCREEN)
-        self.__cat = Cat(self.__surface, ["Graphics/cat.png", ""], (0, 0))
-        self.__player1 = Player(self.__surface, ["Graphics/bat.png", "Graphics/bat_outline.png"],
-                                (50, self.__surface.get_height() // 2))
-        self.__player2 = Player(self.__surface, ["Graphics/bat.png", "Graphics/bat_outline.png"],
-                                (self.__surface.get_width() - 50, self.__surface.get_height() // 2))
-        self.__ball = Ball(self.__surface, ["Graphics/ball.png", "Graphics/ball_outline.png"],
-                           (self.__surface.get_width() // 2, self.__surface.get_height() // 2))
-        # [(FPS text)]
-        self.__text_objects: List[Any] = [()]  # acts as an array for all the text objects
+
+        self.__sprite_manager = SpriteManager()
+        self.__sprite_manager.add_objects("Cat", Cat, 1, self.__surface, ["Graphics/cat.png", ""], (0, 0))
+
+        self.__sprite_manager.add_objects("Player", Player, 2, self.__surface,
+                                          ["Graphics/bat.png", "Graphics/bat_outline.png"],
+                                          (0, 0))
+        self.__sprite_manager.object_pool["Player"][0].move_pos(50, self.__surface.get_height() // 2)
+        self.__sprite_manager.object_pool["Player"][1].move_pos(self.__surface.get_width() - 50,
+                                                                self.__surface.get_height() // 2)
+
+        self.__sprite_manager.add_objects("Ball", Ball, 1, self.__surface,
+                                          ["Graphics/ball.png", "Graphics/ball_outline.png"],
+                                          (0, 0))
+        self.__sprite_manager.object_pool["Ball"][0].move_pos(self.__surface.get_width() // 2,
+                                                              self.__surface.get_height() // 2)
+
+        self.__sprite_manager.add_objects("Text", Text, 1, self.__surface,
+                                          "", "Fonts/Arcadepix.TTF", 16, (0, 0), (0, 0, 0))
+        self.__sprite_manager.object_pool["Text"][0].move_pos(-5, 5)
 
     def __countdown(self) -> None:
         ...
@@ -103,33 +169,9 @@ class Game:
         """
         Resets the positions of the players and the ball to the centre of the screen.
         """
-        self.__player1.draw(True)
-        self.__player2.draw(True)
-        self.__ball.draw(True)
-
-    def __write2screen(self, message: str, font_dir: str, size: int, pos: Tuple[int, int],
-                       color: Tuple[int, int, int]) -> None:
-        """
-        Writes a message with the passed parameters onto the screen.
-        :param str message: Message to put on the screen
-        :param str font_dir: Type of font we want to use
-        :param int size: Font size
-        :param Tuple[int, int] pos: Where on the screen we want to place the message
-        :param Tuple[int, int, int] color: The colour of the text
-        """
-        if self.__text_objects[0]:
-            self.__text_objects[0][0].fill(WHITE)
-            self.__surface.blit(self.__text_objects[0][0], self.__text_objects[0][1])
-            pygame.display.update(Rect(self.__text_objects[0][1][0], self.__text_objects[0][1][1],
-                                       self.__text_objects[0][0].get_width(),
-                                       self.__text_objects[0][0].get_height()))
-
-        font_surf = Font(font_dir, size).render(message, False, color).convert_alpha(self.__surface)
-        pos = (self.__surface.get_width() + pos[0] - font_surf.get_width() if pos[0] < 0 else pos[0],
-               self.__surface.get_height() + pos[1] - font_surf.get_height() if pos[1] < 0 else pos[1])
-        self.__surface.blit(font_surf, pos)
-        pygame.display.update(Rect(pos[0], pos[1], font_surf.get_width(), font_surf.get_height()))
-        self.__text_objects[0] = (font_surf, pos)
+        self.__sprite_manager.object_pool["Player"][0].draw(True)  # Player 1
+        self.__sprite_manager.object_pool["Player"][1].draw(True)  # Player 2
+        self.__sprite_manager.object_pool["Ball"][0].draw(True)  # Ball 1
 
     def __check_events(self) -> None:
         """
@@ -146,6 +188,8 @@ class Game:
         self.__check_events()
         self.__reset_game()
 
+        self.__sprite_manager.object_pool["Text"][0].update_message(f"{FPS_CLOCK.get_fps():0.2f} FPS")  # update FPS
+
     def run(self) -> None:
         """
         Method to call, in order to run the game
@@ -155,7 +199,6 @@ class Game:
 
         while self.__running:
             self.__process()
-            self.__write2screen(f"{FPS_CLOCK.get_fps():0.2f} FPS", "Fonts/Arcadepix.TTF", 16, (-5, 5), (0, 0, 0))
             FPS_CLOCK.tick(FPS)
 
 
